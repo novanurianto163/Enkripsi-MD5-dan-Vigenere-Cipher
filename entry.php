9d399bfca269ad83ec604b4a92ea0fbfbf9f015f
novanurianto163 <novanurianto20@gmail.com>
2020-06-19 21:03:27 +0700

    Enkripsi MD5 dan Vigenere Cipher

--- entry.php
+++ entry.php
@@ -9,7 +9,7 @@
  <!DOCTYPE html>  
  <html>  
       <head>  
-           <title>PHP Form Login dan Daftar menggunkan Enkripsi Password md5</title>  
+           <title>PHP Form Login dan Daftar menggunakan Enkripsi Password md5</title>  
            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
@@ -17,7 +17,7 @@
       <body>  
            <br /><br />  
            <div class="container" style="width:500px;">  
-                <h3 align="center">PHP Form Login dan Daftar menggunkan Enkripsi Password md5</h3>  
+                <h3 align="center">PHP Form Login dan Daftar menggunakan Enkripsi Password md5</h3>  
                 <br />  
                 <?php  
                 echo '<h1>Welcome - '.$_SESSION["username"].'</h1>';  
--- index.php
+++ index.php
@@ -123,7 +123,7 @@
  <!DOCTYPE html>  
  <html>  
       <head>  
-           <title>PHP Form Login dan Daftar menggunkan Enkripsi Password md5 </title>  
+           <title>PHP Form Login dan Daftar menggunakan Enkripsi Password md5 dan Vigenere Cipher</title>  
            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
